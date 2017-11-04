ab9776c98c51b88d784db0929ad4ef986ccfb008
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-04 06:43:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\d{10}/) == nil ? false : true
+  phone.match(/\d{10}/) == nil || phone.match(/\(\d{3}\)\d{7}/) == nil || phone.match(/\(\d{3}\)\d{3}-\d{4}/) == nil || phone.match(/\d{3} \d{3} \d{4}/)== nil ? false : true
 end
