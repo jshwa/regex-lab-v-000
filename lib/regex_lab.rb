eee9d5f133ee5e5fac5b947417c25bc93f808c8c
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-04 06:28:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,9 +12,8 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z]/) == nil || text.match(/[\?\.\!]$/) == nil ? false : true
-
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\d{9}/)
 end
